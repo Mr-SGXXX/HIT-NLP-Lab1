ed67b64cecfc46c52deac3e713f3090600499e32
Mr_SGXXX <2998480931@qq.com>
2021-11-22 19:36:29 +0800

    v1.4.0 - 修改字典结构，完成FMM、BMM

--- lab1_1.py
+++ lab1_1.py
@@ -1,17 +1,25 @@
 # 实验一第一部分 - 词典的构建
 
 DICT = {}
+WORD_LEN_MAP = {}
 DATA_PATH_LIST = ["./res/199801_seg&pos.txt"]
 DICT_PATH = "./res/dic.txt"
-
+MAX_WORD_LEN = 0
 TOTAL_WORD_NUM = 0
 
+
 def load_by_line(string):
-    global TOTAL_WORD_NUM
+    global TOTAL_WORD_NUM, DICT, WORD_LEN_MAP, MAX_WORD_LEN
     word_list = string.split()
     for word in word_list:
         parts = word.split("/")
         if not DICT.__contains__(parts[0]):
+            if len(parts[0]) > MAX_WORD_LEN:
+                MAX_WORD_LEN = len(parts[0])
+            if not WORD_LEN_MAP.__contains__(len(parts[0])):
+                WORD_LEN_MAP[len(parts[0])] = [parts[0]]
+            else:
+                WORD_LEN_MAP[len(parts[0])].append(parts[0])
             DICT[parts[0]] = [parts[1], 1]
         elif not DICT[parts[0]].__contains__(parts[1]):
             DICT[parts[0]].append(parts[1])
@@ -23,18 +31,22 @@ def load_by_line(string):
 
 
 def output_dict():
+    global DICT, WORD_LEN_MAP, MAX_WORD_LEN
     dict_file = open(DICT_PATH, "w")
     i = 0
-    for key in DICT:
-        line = key + ":"
-        for value in DICT[key]:
-            if i % 2 == 0:
-                line += value + " "
-                i = 1
-            else:
-                line += str(value / TOTAL_WORD_NUM) + " "
-                i = 0
-        dict_file.write(line + '\n')
+    for j in range(MAX_WORD_LEN):
+        if WORD_LEN_MAP.get(j):
+            dict_file.write(str(j) + '\n')
+            for key in WORD_LEN_MAP.get(j):
+                line = key + ":"
+                for value in DICT[key]:
+                    if i % 2 == 0:
+                        line += value + " "
+                        i = 1
+                    else:
+                        line += str(value / TOTAL_WORD_NUM) + " "
+                        i = 0
+                dict_file.write(line + '\n')
     dict_file.close()
 
 
@@ -43,8 +55,7 @@ if __name__ == "__main__":
         Data_file = open(Data_path, 'r')
         Str = Data_file.readline()
         while Str != '':
-            Str = Data_file.readline()
             load_by_line(Str)
+            Str = Data_file.readline()
         Data_file.close()
     output_dict()
-
--- lab1_2.py
+++ lab1_2.py
@@ -1,38 +1,83 @@
 # 实验一第二部分 - 正反向最大匹配分词实现
 
-DATA_FILE = "./res/199801_sent.txt"
+DATA_PATH = "./res/199801_sent.txt"
 DICT_PATH = "./res/dic.txt"
 SEG_FMM_PATH = "./res/seg_FMM.txt"
 SEG_BMM_PATH = "./res/seg_BMM.txt"
-
 DICT = []
 MAX_LEN = 0
+
+
 # LONGEST_STR = ''
 
 
 def load_dict(dict_path):
-    global DICT, MAX_LEN, LONGEST_STR
+    global DICT, MAX_LEN  # , LONGEST_STR
     dict_file = open(dict_path, 'r')
     line = dict_file.readline()
+    last_len = 0
     while line != '':
-        part = line.split(":")
-        DICT.append(part[0])
-        if len(part[0]) > MAX_LEN:
-            MAX_LEN = len(part[0])
-            # LONGEST_STR = part[0]
-        # DICT.append(part[1].split()[::2])
-        line = dict_file.readline()
+        try:
+            length = int(line)
+            while last_len != length:
+                DICT.append([])
+                last_len += 1
+            line = dict_file.readline()
+        except ValueError:
+            part = line.split(":")
+            DICT[length - 1].append(part[0])
+            # DICT[length - 1].append(part[1].split()[::2])
+            if len(part[0]) > MAX_LEN:
+                MAX_LEN = len(part[0])
+                # LONGEST_STR = part[0]
+            line = dict_file.readline()
     dict_file.close()
 
 
-def FMM():
-    pass
+def FMM(line):
+    rst = ""
+    while len(line) > 0:
+        cur_len = MAX_LEN if MAX_LEN < len(line) else len(line)
+        try_word = line[:cur_len]
+        while cur_len > 1:
+            if try_word not in DICT[cur_len - 1]:
+                try_word = try_word[:len(try_word) - 1]
+            else:
+                break
+            cur_len -= 1
+        rst = rst + try_word + '/ '
+        line = line[len(try_word):]
+    return rst
 
 
-def BMM():
-    pass
+def BMM(line):
+    rst = ""
+    while len(line) > 0:
+        cur_len = MAX_LEN if MAX_LEN < len(line) else len(line)
+        try_word = line[len(line) - cur_len:]
+        while cur_len > 1:
+            if try_word not in DICT[cur_len - 1]:
+                try_word = try_word[1:]
+            else:
+                break
+            cur_len -= 1
+        rst = try_word + '/ ' + rst
+        line = line[:len(line) - len(try_word)]
+    return rst
 
 
 if __name__ == "__main__":
     load_dict(DICT_PATH)
-    pass
+    Data_file = open(DATA_PATH, 'r')
+    Out_FMM_file = open(SEG_FMM_PATH, 'w')
+    Out_BMM_file = open(SEG_BMM_PATH, 'w')
+    Line = Data_file.readline()
+    while Line != '':
+        Rst_FMM = FMM(Line.strip('\n'))
+        Rst_BMM = BMM(Line.strip('\n'))
+        Out_FMM_file.write(Rst_FMM + '\n')
+        Out_BMM_file.write(Rst_BMM + '\n')
+        Line = Data_file.readline()
+    Out_FMM_file.close()
+    Out_BMM_file.close()
+    Data_file.close()
